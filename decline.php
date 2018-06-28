9a6365312837b7b146da500703fa0358feb35f15
Kurt <kn@responsekings.com>
2018-06-28 10:03:54 +0800

    initial commit

---
+++ decline.php
@@ -0,0 +1,17 @@
+<?php 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	echo "<pre>";
+	print_r($_POST); 
+	echo "</pre>"; 
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Decline</title>
+	</head>
+	<body>
+		<h1>Decline</h1>
+		<a href="index.html">Back to main page</a>
+	</body>
+</html>
---
+++ success.php
@@ -0,0 +1,17 @@
+<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		echo "<pre>";
+		print_r($_POST); 
+		echo "</pre>"; 
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Success</title>
+	</head>
+	<body>
+		<h1>Success</h1>
+		<a href="index.html">Back to main page</a>
+	</body>
+</html>
